c4ea8e547b5025340ca37081b7b13cc6cac63def
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-04-03 21:19:26 +0600

    installed tailwind

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <div className='grid grid-cols-2 bg-gray-400'>
+                <div className=''>
+                    <h1>BUY THE BEST CLOTHS</h1>
+                    <p>You can find various types of clothing stuff in this website. We always make sure that the quality of our products remain top notch. And customer satisfaction if our top priority. Don't have to believe us, just rome around the reviews section and you will know. Happy Shopping !</p>
+                </div>
+                <div className='bg-gray-600'>
+                    <h1>hey</h1>
+                    <img src="./cloths.png" alt="" />
+                </div>
+            </div>
+            <h1>Reviews</h1>
+            <div>
+
+            </div>
         </div>
     );
 };
---
+++ src/hooks/useReviews.js
@@ -0,0 +1,8 @@
+import { useState } from "react"
+
+const useReviews = () => {
+    const [reviews, setReviews] = useState([]);
+    useState(() => {
+
+    }, [])
+}

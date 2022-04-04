1d15610a5ca03df656a7adb198f6740c8d2c298d
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-04-04 15:49:40 +0600

    home responsive done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -15,18 +15,18 @@ const Home = () => {
     }
     return (
         <div className='mt-12 pb-36'>
-            <div className='grid grid-cols-2 mb-12'>
-                <div className='text-left p-4'>
+            <div className='grid lg:grid-cols-2 grid-cols-1 mb-12'>
+                <div className='lg:text-left text-justify p-4  lg:ml-0 ml-8'>
                     <h1>BUY THE BEST <span className='text-red-500'>CLOTHS</span></h1>
                     <p className='italic pr-12'>You can find various types of clothing stuff in this website. We always make sure that the quality of our products remain top notch. And customer satisfaction if our top priority. Don't have to believe us, just rome around the reviews section and you will know. Happy Shopping !</p>
                 </div>
-                <div>
-                    <img className="w-3/4 ml-28 rounded-3xl" src={image} alt="" />
+                <div className='lg:ml-0 mx-auto w-3/4'>
+                    <img className="lg:w-3/4 lg:ml-28 rounded-3xl" src={image} alt="" />
                 </div>
             </div>
             <div>
                 <h1><span className='text-red-500'>R</span>eviews [{homeReviews.length}] </h1>
-                <div className='grid grid-cols-3 gap-3 px-5 mt-8 mb-4'>
+                <div className='grid lg:grid-cols-3 grid-cols-1 gap-3 px-5 mt-8 mb-4'>
                     {
                         homeReviews.map(review => <Review
                             key={review.id}
--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -5,11 +5,11 @@ const Review = (props) => {
     const { image, name, review, rating } = props.review;
     return (
         <div>
-            <div className='flex items-center border-2 border-red-500 text-left p-3 rounded-lg bg-red-100'>
-                <div>
-                    <img className='w-36 h-36 rounded-full' src={image} alt="" />
+            <div className='flex lg:flex-row items-center flex-col border-2 lg:h-40 h-80 border-red-500 text-left lg:p-2 p-3 rounded-lg bg-red-100'>
+                <div className='lg:w-1/4 w-1/2 mr-4'>
+                    <img className='rounded-full' src={image} alt="" />
                 </div>
-                <div className='ml-4'>
+                <div className='lg:pt-3  w-full pl-5 pt-3'>
                     <h3>{name}</h3>
                     <p className='italic font-semibold'>{review}</p>
                     <p className='italic'><small>Rating : <span className='text-red-500'>{rating}</span></small></p>
